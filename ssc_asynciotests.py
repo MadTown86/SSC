359cc0e60cd2fe3e80cefec0b1edb675efad6dc2
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-04-25 16:07:05 -0500

    From-Work Update

--- ssc_asynciotests.py
+++ ssc_asynciotests.py
@@ -1,18 +1,24 @@
+import asyncio
+
 import simplestockchecker_fetchtool
 import unittest
 from unittest.mock import Mock
 from unittest.mock import patch
 
 
+
 class Tester1(unittest.IsolatedAsyncioTestCase):
 
     # @patch('simplestockchecker_fetchtool.FetchUrlRequestShelfSSC')
     @patch('requests.request')
-    @patch("requests.Response")
     # @patch('simplestockchecker_fetchtool.FetchDataStoreSSC')
-    async def test_fetchcycler(self, mock_request, MockResponse): #MockFetchStore, mock_request, MockFetchUrl
-        mock_request = Mock(spec_set="requests.request")
-        mock_request.configure_mock(**{"status_code": 200, "response": "responsetext"})
+    async def test_fetchcycler(self, mock_request): #MockFetchStore, mock_request, MockFetchUrl
+        m = mock_request()
+        stcode = unittest.mock.PropertyMock(return_value=200)
+        jsonmock = unittest.mock.PropertyMock(return_value={"JSONDICT": {"URL": "value1", "qd": "value2"}})
+        type(m).status_code = stcode
+        type(m).text = jsonmock
+
         #mock_request.return_value = Mock(status_code=200, response={"data_get": {"id": "test"}})
         #MFURLSHELF = MockFetchUrl()
         #MFS = MockFetchStore()
@@ -24,8 +30,8 @@ class Tester1(unittest.IsolatedAsyncioTestCase):
                                                   #"url_sectordata": {"url": "url_sectordata", "qs": "qs_sector", "headers": "headers"}}
 
         FC1T = simplestockchecker_fetchtool.FetchCyclerSSC("MSFT")
-
-        await FC1T.rapid_fetch({"url_income": {"url": "urltest1", "qs": "qstest1", "headers": "headerstest1"}})
+        loop = asyncio.get_event_loop()
+        await FC1T.rapid_fetch({"TESTDICT": "MOCKERY"})
         #print(MFURLSHELF.pullfetchshelf())
         #await FC1T.rapid_fetch(MFURLSHELF.pullfetchshelf())
         #mock_request.assert_called()
