f1d6abebd27aaec602b54cee16d81e5769325519
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-07-26 04:54:20 -0500

    Work Laptop Commit - Adding shelfpullssc, mainly for testing purposes.  Frequently checking shelves to ensure data is parsed and stored in correct format.  In future projects, should add simple methods to a super class and have subclasses inherit.

---
+++ sscpackage/tests_ssc/test_shelfpullssc.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_shelfpullssc(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
