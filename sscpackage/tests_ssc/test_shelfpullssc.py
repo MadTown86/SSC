57ce65eda353dd760304d7a4ef1dfa322e9be958
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-07-31 14:18:02 -0500

    Finished - test_shelfpullssc, merged from work.  Now moving to plan grading process.

--- sscpackage/tests_ssc/test_shelfpullssc.py
+++ sscpackage/tests_ssc/test_shelfpullssc.py
@@ -1,9 +1,21 @@
 import unittest
+import shelfpullssc
+
 
 
 class MyTestCase(unittest.TestCase):
     def test_shelfpullssc(self):
-        self.assertEqual(True, False)  # add assertion here
+        pathtoshelve = 'C:\\SSC\\SimpleStockChecker_REV1\\sscpackage\\storage\\testshelvessc'
+
+        first_resssc = {'First Key': {'FK1': 'FV1', 'FK2': 'FV2'}, 'Second Key': {'FK22': 'FV22', 'FK23': 'FV23'}}
+        second_resssc = ['First Key', 'Second Key']
+        third_resssc = {'FK1': 'FV1', 'FK2': 'FV2'}
+        keyfortestssc = 'First Key'
+
+        SV = shelfpullssc.ShelfPullSSC()
+        self.assertEqual(SV.pullshelfssc(pathtoshelve), first_resssc)
+        self.assertEqual(SV.pullshelfkeylistssc(pathtoshelve), second_resssc)
+        self.assertEqual(SV.pullvalueinshelfssc(pathtoshelve, keyfortestssc), third_resssc)
 
 
 if __name__ == '__main__':
