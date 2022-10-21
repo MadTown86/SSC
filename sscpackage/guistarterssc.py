4b56999305321a8e053b79d98a115ab9eebeef68
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-10-21 15:12:41 -0500

    Updating type annotations.

--- sscpackage/guistarterssc.py
+++ sscpackage/guistarterssc.py
@@ -354,14 +354,11 @@ class GuiStarterSSC(object):
                             schedule.clear()
 
                         # Delete 'failed fetches' from FetchLog
-                        try:
                             fail_list = fetchssc.FetchSSC().pull_tickerfail()
                             FLOG = fetchlogssc.FetchLogSSC()
                             for ticker, uniqueid in fail_list:
                                 FLOG.ssc_fetchlogdeleted(ticker, uniqueid)
-                        except Exception as er:
-                            print("Exception in guistarter -> fetchlogdelete")
-                            print(er)
+
 
                         print("After Fetch")
                         GuiStarterSSC.end_fetchstart = True
